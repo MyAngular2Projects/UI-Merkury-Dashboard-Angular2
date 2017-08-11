98fe0384cc24de1595ce1f6e3704aa67f1b9720b
yuda85 <yuda8855@gmail.com>
2017-08-12 00:07:49 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+
 import { Component } from '@angular/core';
+declare var $:any; 
+declare var jQuery: any;
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private sideNavInvoke(){
+  $(".button-collapse").sideNav()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 
+import { ReportComponent } from './report/report.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ReportsComponent } from './reports/reports.component';
+import { SalesComponent } from './sales/sales.component';
+import { WorkflowComponent } from './workflow/workflow.component';
+import { ActivityComponent } from './activity/activity.component';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    ReportComponent,
+    TasksComponent,
+    MessagesComponent,
+    ReportsComponent,
+    SalesComponent,
+    WorkflowComponent,
+    ActivityComponent
+
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
